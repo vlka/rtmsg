533753991173bbecdc3ca943b7f6d2de95d2fa48
vlka <vladimir_kashkin@bk.ru>
2014-11-19 22:34:55 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
-
-app.listen(80);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+console.log('77777');
+////var app = require('http').createServer(handler)
+//var io = require('socket.io')(app);
+//var fs = require('fs');
+//
+//app.listen(80);
+//
+//function handler (req, res) {
+//  fs.readFile(__dirname + '/index.html',
+//  function (err, data) {
+//    if (err) {
+//      res.writeHead(500);
+//      return res.end('Error loading index.html');
+//    }
+//
+//    res.writeHead(200);
+//    res.end(data);
+//  });
+//}
+//
+//io.on('connection', function (socket) {
+//  socket.emit('news', { hello: 'world' });
+//  socket.on('my other event', function (data) {
+//    console.log(data);
+//  });
+//});
