e1a4a0bcd14b37aceabfbba6cbca9168d299bcca
vlka <vladimir_kashkin@bk.ru>
2014-11-19 21:30:44 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- Rtm.php
+++ Rtm.php
@@ -4,5 +4,9 @@ namespace vlka\rtmsg;
 use yii\base\Component;
 class Rtm extends Component
 {
-	
+	public function register($messages = null){
+		echo '<pre>';
+		print_r($messages);
+		echo '</pre>';
+	}
 }
