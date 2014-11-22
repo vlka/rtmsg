d0c0ec2ba266a4e7f9687e79d891e02962f79bfe
vlka <vladimir_kashkin@bk.ru>
2014-11-22 19:34:07 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- console/RtmsgController.php
+++ console/RtmsgController.php
@@ -7,4 +7,8 @@ class RtmsgController extends Controller
 	public function actionTest(){
 		echo "test 777";
 	}
+	
+	public function actionInit(){
+		$test = $this->confirm('Введите тестовое слово');
+	}
 }
