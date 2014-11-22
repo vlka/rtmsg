e5231730705ba3a74c2dfc149eaf435085933c3f
vlka <vladimir_kashkin@bk.ru>
2014-11-22 20:14:55 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- BootConsole.php
+++ BootConsole.php
@@ -8,13 +8,11 @@ class BootConsole extends Component
 	public function init() {
 		parent::init();
 		Yii::$app->controllerMap['rtmsg'] = 'vlka\rtmsg\console\RtmsgController';
-		Yii::$app->i18n->translations['rtmsg'] = [
-			'rtmsg*' => [
-                'class' => 'yii\i18n\PhpMessageSource',
-                'fileMap' => [
-                    'rtmsg' => 'rtmsg.php',
-                ],
-            ],
+		Yii::$app->i18n->translations['rtmsg*'] = [
+			'class' => 'yii\i18n\PhpMessageSource',
+			'fileMap' => [
+				'rtmsg' => 'rtmsg.php',
+			],
 		];
 	}
 }
