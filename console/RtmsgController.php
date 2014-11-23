adfe236f0e184597bccfb4d7e14e41ba9169a02b
vlka <vladimir_kashkin@bk.ru>
2014-11-23 16:38:53 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- console/RtmsgController.php
+++ console/RtmsgController.php
@@ -14,6 +14,7 @@ class RtmsgController extends Controller
 		}
 		exec('cp -R ' . Yii::getAlias('@vendor/vlka/rtmsg/node_modules') . ' ' . Yii::getAlias($rtm->node_path));
 		exec('cp ' . Yii::getAlias('@vendor/vlka/rtmsg/server.js') . ' ' . Yii::getAlias($rtm->node_path));
+		exec('cp ' . Yii::getAlias('@vendor/vlka/rtmsg/package.json') . ' ' . Yii::getAlias($rtm->node_path));
 		$conf = 'exports.port = ' . $rtm->node_port . ";\n";
 		file_put_contents(Yii::getAlias($rtm->node_path) . '/conf.js', $conf);
 	}
