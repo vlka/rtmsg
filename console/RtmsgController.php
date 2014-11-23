6d365323bce9708888a38a0652065ce3fb9cffa0
vlka <vladimir_kashkin@bk.ru>
2014-11-23 15:10:39 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- console/RtmsgController.php
+++ console/RtmsgController.php
@@ -4,14 +4,16 @@ namespace vlka\rtmsg\console;
 use Yii;
 use yii\console\Controller;
 use vlka\rtmsg\Rtm;
+use yii\helpers\Json;
 class RtmsgController extends Controller
 {
 	public function actionInit(){
 		$rtm = new Rtm();
-//		if(file_exists($this->node_path)){
-//			exec('rm -R '.Yii::getAlias($rtm->node_path));
-//		}
-//		mkdir(Yii::getAlias($rtm->node_path));
-		exec('cp -rf '.Yii::getAlias('@vendor/vlka/rtmsg/node_modules').' '.Yii::getAlias($rtm->node_path));
+		exec('cp -rf ' . Yii::getAlias('@vendor/vlka/rtmsg/node_modules') . ' ' . Yii::getAlias($rtm->node_path));
+		exec('cp ' . Yii::getAlias('@vendor/vlka/rtmsg/server.js') . ' ' . Yii::getAlias($rtm->node_path));
+		$conf = Json::encode([
+			'port' => $rtm->node_port,
+		]);
+		file_put_contents(Yii::getAlias($rtm->node_path) . '/conf.js', $conf);
 	}
 }
